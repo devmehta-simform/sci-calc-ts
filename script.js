bd7b256cf9bb3bc6e0171e41d60f3f8e0b6a9eb9
dev mehta <dev.mehta@simformsolutions.com>
2025-01-16 17:37:58 +0530

    feat(infToPost): added simple infix to postfix function

--- script.js
+++ script.js
@@ -120,5 +120,63 @@ clrbtn.addEventListener("click", (e) => {
   input.value = "";
 });
 eqbtn.addEventListener("click", (e) => {
-  console.log("eq");
+  // console.log(
+  //   "eq",
+
+  // );
+  const infCharArray = input.value
+    .toString()
+    .split(/\s*([\(\)+\-*/^])\s*/)
+    .filter((c) => c != "");
+  console.log(infCharArray);
+
+  const postCharArray = infToPost(infCharArray);
+  console.log(postCharArray);
+
+  function getPrec(ch) {
+    switch (ch) {
+      case "+":
+      case "-":
+        return 1;
+      case "*":
+      case "/":
+        return 2;
+      case "^":
+        return 3;
+      default:
+        return -1;
+    }
+  }
+  function infToPost(str) {
+    const stack = [];
+    const ans = [];
+    for (const c of str) {
+      if (!Number.isNaN(parseFloat(c))) {
+        ans.push(c);
+        continue;
+      } else if (c == "(") {
+        stack.push(c);
+        continue;
+      } else if (c == ")") {
+        while (stack.length != 0 && stack.at(stack.length - 1) != "(") {
+          ans.push(stack.pop());
+        }
+        if (stack.length == 0) {
+          alert("error invalid input");
+          break;
+        } else stack.pop();
+        continue;
+      } else {
+        while (
+          stack.length != 0 &&
+          getPrec(stack.at(stack.length - 1)) >= getPrec(c)
+        ) {
+          ans.push(stack.pop());
+        }
+        stack.push(c);
+      }
+    }
+    while (stack.length != 0) ans.push(stack.pop());
+    return ans;
+  }
 });
